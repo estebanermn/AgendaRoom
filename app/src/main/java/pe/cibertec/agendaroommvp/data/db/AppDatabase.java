5838e461936eac158161cc372e0e271179e29b90
EXTENSION <estebanermn@gmail.com>
2019-06-30 13:30:22 -0500

    mvp

---
+++ app/src/main/java/pe/cibertec/agendaroommvp/data/db/AppDatabase.java
@@ -0,0 +1,25 @@
+package pe.cibertec.agendaroommvp.data.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import pe.cibertec.agendaroommvp.data.db.model.Contact;
+
+@Database(entities = {Contact.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase instance = null;
+
+    public static AppDatabase getInstance(Context contexto) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(contexto, AppDatabase.class, "agenda.db")
+                    .build();
+        }
+        return instance;
+    }
+
+    public abstract ContactDao getContactDao();
+
+}
---
+++ app/src/main/java/pe/cibertec/agendaroommvp/data/db/ContactDao.java
@@ -0,0 +1,31 @@
+package pe.cibertec.agendaroommvp.data.db;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+import pe.cibertec.agendaroommvp.data.db.model.Contact;
+
+@Dao
+public interface ContactDao {
+
+    @Insert
+    void insertContacts(Contact... contacts);
+
+    @Update
+    void updateContact(Contact contact);
+
+    @Delete
+    void deleteContact(Contact contact);
+
+    @Query("select * from Contact")
+    List<Contact> getAll();
+
+    @Query("select * from Contact where:nombre")
+    List<Contact> searchByName(String nombre);
+
+}
